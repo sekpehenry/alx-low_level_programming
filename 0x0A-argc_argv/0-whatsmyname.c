74c9ea4e9e1cd4092845cb5efdb56944ee908589
sekpehenry <sekpehenry15@email.com>
2023-03-12 15:13:10 -0700

    whatsmyname commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+print_name_of_funtion.c
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+        printf("%s\n", argv[argc - argc]);
+        return (0);
+}
