02f4fb70af58b091c9375e8462010cd958ea26c9
sekpehenry <sekpehenry15@email.com>
2023-03-12 15:32:56 -0700

    print number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+me_of_funtion.c
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argc: offset count
+ *@argv: offset value
+ *Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	        printf("%d\n",argc);
+		        return (0);
+}
