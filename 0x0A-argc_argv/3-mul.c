502b5a7a8e2196f864a854cd69ae6c4f79b065b3
sekpehenry <sekpehenry15@email.com>
2023-03-12 17:28:59 -0700

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 
 /**
- * main - function
- * @argc: offset count
- * @argv: offset value
+ * main - multiplies two numbers
+ * @argc: n args
+ * @argv: arr args
  * Return: 0
  */
-
-
 int main(int argc, char *argv[])
 {
-        if (argc !=3)
-        {
-                printf("Error\n");
-                return (1);
-        }
+	int i, val = 1;
 
-        int sum;
-	sum = atoi(argv[1]) * atoi(argv[2]);
-	printf ("%d\n", sum);
-        return 0;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		val *= atoi(argv[i]);
+	}
+	printf("%d\n", val);
+	return (0);
 }
