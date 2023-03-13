bc724292987cc8bcb0077ce1e1fc1c2ade1762f5
sekpehenry <sekpehenry15@email.com>
2023-03-12 17:12:19 -0700

    Multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+        if (argc !=3)
+        {
+                printf("Error");
+                return (1);
+        }
+
+        printf ("%d\n", atoi(argv[1]) * ato>
+        return 0;
+}
